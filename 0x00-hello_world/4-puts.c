4e3ecc37b89e25b7c68f1b7b124948477aff44c7
PelumiDada <pearlpeluz@gmail.com>
2023-10-06 02:02:12 +0100

    fixed put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
 *main-Entry point
 *
-*Return Always 0 (Success)
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-puts("\"Programming is like builing a multilinqual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
